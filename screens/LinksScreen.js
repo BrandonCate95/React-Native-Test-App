3e4279a36b2340e15dd4f8cf062f0e6e1ab365f5
Brandon Cate <brandoncate95@gmail.com>
2019-01-06 19:34:55 -0800

    tried adding the global getTheme function, to Added search bar / cards / scroll view to explore page.

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,6 +1,11 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
-import { ExpoLinksView } from '@expo/samples';
+import { ScrollView, StyleSheet, Platform, Image } from 'react-native';
+import { StyleProvider, Container, Header, Item, Input, Icon, Button, Text, Content, Card, CardItem, Thumbnail, Left, Body, Right } from 'native-base';
+
+import getTheme from '../native-base-theme/components'
+import custom from '../native-base-theme/variables/Custom'
+
+var ar = [1,2,3,4]
 
 export default class LinksScreen extends React.Component {
   static navigationOptions = {
@@ -9,11 +14,54 @@ export default class LinksScreen extends React.Component {
 
   render() {
     return (
-      <ScrollView style={styles.container}>
-        {/* Go ahead and delete ExpoLinksView and replace it with your
-           * content, we just wanted to provide you with some helpful links */}
-        <ExpoLinksView />
-      </ScrollView>
+          <Container>
+            <Header searchBar rounded style={{backgroundColor: '#efefef', marginTop: 15}}>
+              <Item>
+                <Icon name={Platform.OS === 'ios' ? 'ios-search' : 'md-search'} />
+                <Input placeholder="Search" />
+                <Icon name={Platform.OS === 'ios' ? 'ios-people' : 'md-people'} />
+              </Item>
+              {Platform.OS === 'ios' &&
+                <Button transparent>
+                  <Text>Search</Text>
+                </Button>
+              }
+            </Header>
+            <ScrollView>
+              <Content>
+                {ar.map((num) =>             
+                  <Card key={num}>
+                    <CardItem>
+                      <Left>
+                        <Thumbnail source={{uri: 'https://nativebase.io/assets/img/front-page-icon.png'}} />
+                        <Body>
+                          <Text>NativeBase</Text>
+                          <Text note>GeekyAnts</Text>
+                        </Body>
+                      </Left>
+                    </CardItem>
+                    <CardItem cardBody>
+                      <Image source={{uri: 'https://4.bp.blogspot.com/_xl9E-bw7sMg/TM4F7-37b8I/AAAAAAAAAlo/sIQMOd0IpbU/s1600/inside+a+forest_negative.jpg'}} style={{height: 200, width: null, flex: 1}}/>
+                    </CardItem>
+                    <CardItem>
+                      <Left>
+                        <Button transparent>
+                          <Text>12</Text>
+                          <Icon name="thumbs-up" />
+                        </Button>
+                      </Left>
+                      <Right>
+                        <Button transparent>
+                          <Text>4</Text>
+                          <Icon name="chatbubbles" />
+                        </Button>
+                      </Right>
+                    </CardItem>
+                  </Card>
+                )}
+              </Content>
+            </ScrollView>
+          </Container> 
     );
   }
 }
--- screens/profile/OptionsScreen.js
+++ screens/profile/OptionsScreen.js
@@ -1,6 +1,9 @@
-import React from 'react';
-import { Platform, StyleSheet } from 'react-native';
-import { Container, Header, Content, Button, List, ListItem, Text, Icon, Left, Body, Right, View } from 'native-base';
+import React from 'react'
+import { Platform, StyleSheet } from 'react-native'
+import { Header, StyleProvider, Container, Content, Button, List, ListItem, Text, Icon, Left, Body, Right, View } from 'native-base'
+
+import getTheme from '../../native-base-theme/components'
+import custom from '../../native-base-theme/variables/Custom'
 
 const styles = StyleSheet.create({
   UserContainer:{
@@ -18,7 +21,7 @@ const styles = StyleSheet.create({
     marginBottom: 10,
   },
   ListItemButton:{
-    backgroundColor: "#FF9501",
+    // backgroundColor: "#FF9501",
   }
 })
 
@@ -29,122 +32,124 @@ export default class OptionsScreen extends React.Component {
 
   render() {
     return(
-      <Container style={{ flex: 1 }}>
-        <Content style={{marginTop: 30}}>
+      // <StyleProvider style={{...getTheme(custom)}}>
+        <Container style={{ flex: 1 }}>
+          <Content style={{marginTop: 30}}>
 
-          <View style={styles.UserContainer}>
-            <Icon 
-              name={Platform.OS === 'ios' ? 'ios-contact' : 'md-contact'} 
-              style={{fontSize: 80, color: "grey"}}
-            />
-            <Text style={styles.Username}>Bobby Bouncer</Text>
-          </View>
+            <View style={styles.UserContainer}>
+              <Icon 
+                name={Platform.OS === 'ios' ? 'ios-contact' : 'md-contact'} 
+                style={{fontSize: 80, color: '#afafaf'}}
+              />
+              <Text style={styles.Username}>Bobby Bouncer</Text>
+            </View>
 
-          <View style={styles.Hr} />
+            <View style={styles.Hr} />
 
-          <List>
+            <List>
 
-            <ListItem icon onPress={() => this.props.navigation.navigate('Settings')}>
-              <Left>
-                <Button style={styles.ListItemButton}>
-                  <Icon active name={Platform.OS === 'ios' ? 'ios-notifications' : 'md-notifications'} />
-                </Button>
-              </Left>
-              <Body>
-                <Text>Notifications</Text>
-              </Body>
-              <Right>
-                <Icon name="arrow-forward" />
-              </Right>
-            </ListItem>
+              <ListItem icon onPress={() => this.props.navigation.navigate('Settings')}>
+                <Left>
+                  <Button style={styles.ListItemButton}>
+                    <Icon active name={Platform.OS === 'ios' ? 'ios-notifications' : 'md-notifications'} />
+                  </Button>
+                </Left>
+                <Body>
+                  <Text>Notifications</Text>
+                </Body>
+                <Right>
+                  <Icon name="arrow-forward" />
+                </Right>
+              </ListItem>
 
-            <ListItem icon onPress={() => this.props.navigation.navigate('Settings')}>
-              <Left>
-                <Button style={styles.ListItemButton}>
-                  <Icon active name={Platform.OS === 'ios' ? 'ios-refresh' : 'md-refresh'} />
-                </Button>
-              </Left>
-              <Body>
-                <Text>Payment History</Text>
-              </Body>
-              <Right>
-                <Icon name="arrow-forward" />
-              </Right>
-            </ListItem>
+              <ListItem icon onPress={() => this.props.navigation.navigate('Settings')}>
+                <Left>
+                  <Button style={styles.ListItemButton}>
+                    <Icon active name={Platform.OS === 'ios' ? 'ios-refresh' : 'md-refresh'} />
+                  </Button>
+                </Left>
+                <Body>
+                  <Text>Payment History</Text>
+                </Body>
+                <Right>
+                  <Icon name="arrow-forward" />
+                </Right>
+              </ListItem>
 
-            <ListItem icon onPress={() => this.props.navigation.navigate('Settings')}>
-              <Left>
-                <Button style={styles.ListItemButton}>
-                  <Icon active name={Platform.OS === 'ios' ? 'ios-gift' : 'md-gift'} />
-                </Button>
-              </Left>
-              <Body>
-                <Text>Promotions</Text>
-              </Body>
-              <Right>
-                <Icon name="arrow-forward" />
-              </Right>
-            </ListItem>
+              <ListItem icon onPress={() => this.props.navigation.navigate('Settings')}>
+                <Left>
+                  <Button style={styles.ListItemButton}>
+                    <Icon active name={Platform.OS === 'ios' ? 'ios-gift' : 'md-gift'} />
+                  </Button>
+                </Left>
+                <Body>
+                  <Text>Promotions</Text>
+                </Body>
+                <Right>
+                  <Icon name="arrow-forward" />
+                </Right>
+              </ListItem>
 
-            <ListItem icon onPress={() => this.props.navigation.navigate('Settings')}>
-              <Left>
-                <Button style={styles.ListItemButton}>
-                  <Icon active name={Platform.OS === 'ios' ? 'ios-settings' : 'md-settings'} />
-                </Button>
-              </Left>
-              <Body>
-                <Text>Settings</Text>
-              </Body>
-              <Right>
-                <Icon name="arrow-forward" />
-              </Right>
-            </ListItem>
+              <ListItem icon onPress={() => this.props.navigation.navigate('Settings')}>
+                <Left>
+                  <Button style={styles.ListItemButton}>
+                    <Icon active name={Platform.OS === 'ios' ? 'ios-settings' : 'md-settings'} />
+                  </Button>
+                </Left>
+                <Body>
+                  <Text>Settings</Text>
+                </Body>
+                <Right>
+                  <Icon name="arrow-forward" />
+                </Right>
+              </ListItem>
 
-            <ListItem icon onPress={() => this.props.navigation.navigate('Settings')}>
-              <Left>
-                <Button style={styles.ListItemButton}>
-                  <Icon active name={Platform.OS === 'ios' ? 'ios-help-circle-outline' : 'md-help-circle-outline'} />
-                </Button>
-              </Left>
-              <Body>
-                <Text>Help/Support</Text>
-              </Body>
-              <Right>
-                <Icon name="arrow-forward" />
-              </Right>
-            </ListItem>
+              <ListItem icon onPress={() => this.props.navigation.navigate('Settings')}>
+                <Left>
+                  <Button style={styles.ListItemButton}>
+                    <Icon active name={Platform.OS === 'ios' ? 'ios-help-circle-outline' : 'md-help-circle-outline'} />
+                  </Button>
+                </Left>
+                <Body>
+                  <Text>Help/Support</Text>
+                </Body>
+                <Right>
+                  <Icon name="arrow-forward" />
+                </Right>
+              </ListItem>
 
-            <ListItem icon onPress={() => this.props.navigation.navigate('Settings')}>
-              <Left>
-                <Button style={styles.ListItemButton}>
-                  <Icon active name={Platform.OS === 'ios' ? 'ios-information-circle-outline' : 'md-information-circle-outline'} />
-                </Button>
-              </Left>
-              <Body>
-                <Text>Legal</Text>
-              </Body>
-              <Right>
-                <Icon name="arrow-forward" />
-              </Right>
-            </ListItem>
+              <ListItem icon onPress={() => this.props.navigation.navigate('Settings')}>
+                <Left>
+                  <Button style={styles.ListItemButton}>
+                    <Icon active name={Platform.OS === 'ios' ? 'ios-information-circle-outline' : 'md-information-circle-outline'} />
+                  </Button>
+                </Left>
+                <Body>
+                  <Text>Legal</Text>
+                </Body>
+                <Right>
+                  <Icon name="arrow-forward" />
+                </Right>
+              </ListItem>
 
-            <ListItem icon onPress={() => this.props.navigation.navigate('Settings')}>
-              <Left>
-                <Button style={styles.ListItemButton}>
-                  <Icon active name={Platform.OS === 'ios' ? 'ios-exit' : 'md-exit'} />
-                </Button>
-              </Left>
-              <Body>
-                <Text>Sign Out</Text>
-              </Body>
-              <Right>
-                <Icon name="arrow-forward" />
-              </Right>
-            </ListItem>
+              <ListItem icon onPress={() => this.props.navigation.navigate('Settings')}>
+                <Left>
+                  <Button style={styles.ListItemButton}>
+                    <Icon active name={Platform.OS === 'ios' ? 'ios-exit' : 'md-exit'} />
+                  </Button>
+                </Left>
+                <Body>
+                  <Text>Sign Out</Text>
+                </Body>
+                <Right>
+                  <Icon name="arrow-forward" />
+                </Right>
+              </ListItem>
 
-          </List>
-        </Content>
-      </Container>
+            </List>
+          </Content>
+        </Container>
+      // </StyleProvider>
     )
   }
 }
